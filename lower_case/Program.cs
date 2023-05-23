3bd23c7704fc37e9de1d4fef0688d48c9f9de871
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-23 17:37:41 +0300

    added a new file lower case

---
+++ lower_case/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Please enter your name: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "ramiz")
+{
+    Console.WriteLine("Hey! Мой тёска!");
+}
+else
+{
+    Console.WriteLine("Hello " + userName);
+}
