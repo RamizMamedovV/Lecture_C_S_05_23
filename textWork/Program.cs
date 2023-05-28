62eb838b6a47114f4be6c30a53aa60f7bc77503d
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-28 15:15:03 +0300

    added a new file textWork

---
+++ textWork/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан текст. В тексте на русском языке, нужно все пробелы заменить на "-"
+// и маленькие буквы "а" заменить на большие"А"
+
+string text = "Да, вы правы. В моем предыдущем ответе была допущена ошибка."
++ " 'Граб' не является деревом. Я приношу извинения за путаницу."
++ " Корректные переводы названий деревьев, упомянутых в загадке,";
+
+string Replace(string txt, char oldValue, char newValue) {
+    string result = string.Empty;
+
+    for (int i = 0; i < txt.Length; i++)
+    {
+        if(txt[i] == oldValue) result += $"{newValue}";
+        else result += $"{txt[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '-');
+Console.WriteLine(text);
+Console.WriteLine();
+Console.WriteLine(newText);
+Console.WriteLine();
+Console.WriteLine(Replace(newText, 'а', 'А'));
