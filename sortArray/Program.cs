e8c230bbf0ae591a97e87f7dc1d145a837c4b78d
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-05-28 18:40:12 +0300

    modified the file sortArray

--- sortArray/Program.cs
+++ sortArray/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Написать программу, которая отсортирует массив
-// например от мин. значения до макс.
+// например от мин. значения до макс.(вариант 2й: от мак. до мин)
+
+//                      неизменная часть для 2х вариантов
 
 void PrintArray(int[] arr)
 {
@@ -8,6 +10,7 @@ void PrintArray(int[] arr)
         Console.Write($" {arr[i]}");
     }
 }
+//                              вариант 1
 
 void SortArray(int[] arr1)
 {
@@ -29,7 +32,30 @@ void SortArray(int[] arr1)
     }
 }
 
-int[] array = { 2, 6, 8, 8, 4, 1, 1, 0, 0, 4 };
+//                              вариант 2
+
+// void SortArray(int[] arr1)
+// {
+//     for (int i = 0; i < arr1.Length - 1; i++)
+//     {
+//         int maxIndex = i;
+//         for (int j = i + 1; j < arr1.Length; j++)
+//         {
+//             if (arr1[j] > arr1[maxIndex])
+//             {
+//                 maxIndex = j;
+//             }
+//         }
+//         int temp = arr1[maxIndex];
+//         arr1[maxIndex] = arr1[i];
+//         arr1[i] = temp;
+//         Console.WriteLine();
+//         PrintArray(arr1);
+//     }
+// }
+
+
+int[] array = { 2, 6, 5, 8, 8, 4, 1, 1, 0, 0, 4 };
 PrintArray(array);
 
 SortArray(array);
