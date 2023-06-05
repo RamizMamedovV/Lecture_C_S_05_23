c904a73614e4f27ec296279a05f1119567cb6f62
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-06-05 16:57:09 +0300

    added a task Recursion_MultiDimension

---
+++ Recursion_MultiDimension/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// int[,] array = new int[2, 3];
+// ArrFillInt(array);
+
+// void ArrFillInt(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($" {i}{j} -");
+//             //  Console.WriteLine("-");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+string[,] strings = new string[2, 3];
+ArrFillInt(strings);
+
+void ArrFillInt(string[,] str)
+{
+    string str1 = String.Empty;
+    for (int i = 0; i < str.GetLength(0); i++)
+    {
+            str1 = " //Ab-";
+        for (int j = 0; j < str.GetLength(1); j++)
+        {
+            int a = 1;
+            str1 += str1;
+            Console.Write(a + str1);
+        }
+        Console.WriteLine();
+    }
+}
